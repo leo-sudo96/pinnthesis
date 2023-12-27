9bf75c622fcac12024ccdce9f3f570555e538446
leo-sudo96 <leonardbuschmann@gmail.com>
2023-12-27 15:06:02 +0100

    learning issues

--- src/duffing_generator.py
+++ src/duffing_generator.py
@@ -8,23 +8,35 @@ from scipy.integrate import odeint
 import random
 
 class DuffingGeneratorClass:
-    def __init__(self):
-        # Initialization code if needed
-        pass
+    def duffing_generator(self):
+        batches = []
+        for _ in range(64):  # Generate 64 batches
+            # Randomly generate parameters
+            a = random.uniform(-1.5, 1.5)
+            b = random.uniform(-1.5, 1.5)
+            d = random.uniform(-10, 10)
+            gamma = random.uniform(-1.5, 1.5)
+            w = random.uniform(0, 10)
 
-    def duffing_generator(self, x):
-        a = random.uniform(0, 1.5)
-        b = random.uniform(0, 1.5)
-        d = random.uniform(0, 5)
-        gamma = random.uniform(0, 1.5)
-        w = random.uniform(0, 10)
+            # Time points
+            x = torch.linspace(0, 1, 500).view(-1, 1)
 
-        def duffing(y, t):
-            y0, y1 = y
-            dydt = [y1, -d * y1 - a * y0 - b * y0**3 + gamma * np.cos(w * t)]
-            return dydt
+            # Duffing differential equation
+            def duffing(y, t):
+                y0, y1 = y
+                dydt = [y1, -d * y1 - a * y0 - b * y0**3 + gamma * np.cos(w * t)]
+                return dydt
 
-        y0 = [0, 0]
-        sol = odeint(duffing, y0, x.view(-1).numpy())  # Convert x to a one-dimensional numpy array
-        y = torch.tensor(sol[:, 0])
-        return [y, d, a, b, gamma, w, x]
+            # Initial conditions and solving the ODE
+            y0 = [0, 0]
+            sol = odeint(duffing, y0, x.view(-1).numpy())
+            y = torch.tensor(sol[:, 0], dtype=torch.float32)
+
+            # Create a batch as a tuple
+            batch = (y, torch.tensor(d), torch.tensor(a), torch.tensor(b), torch.tensor(gamma), torch.tensor(w), x)
+            
+            # Append the batch to the list of batches
+            batches.append(batch)
+
+
+        return batches
--- src/physics_loss.py
+++ src/physics_loss.py
@@ -3,18 +3,25 @@
 
 # In[ ]:
 import torch
-from torch import autograd
+import numpy as np
+import torch.autograd as autograd
 class physics_loss_class:
-#physics_loss definition
-    def physics_loss(self,model,x_physics,d,a,b,gamma,w):
+   def physics_loss(self, model, x_physics, d_vec, a_vec, b_vec, gamma_vec, w_vec):
+       # Initialize total loss
+       total_loss = 0.0
 
+       # Loop over each parameter vector
+       for d, a, b, gamma, w in zip(d_vec, a_vec, b_vec, gamma_vec, w_vec):
+           # Compute the physics loss by enforcing the differential equation
+           yhp = model(x_physics)
+           dy_pred = autograd.grad(yhp, x_physics, torch.ones_like(yhp), create_graph=True)[0]
+           d2y_pred = autograd.grad(dy_pred, x_physics, torch.ones_like(dy_pred), create_graph=True)[0]
 
-            # Compute the physics loss by enforcing the differential equation
-            yhp = model(x_physics)
-            dy_pred = autograd.grad(yhp, x_physics, torch.ones_like(yhp), create_graph=True)[0]
-            d2y_pred = autograd.grad(dy_pred, x_physics, torch.ones_like(dy_pred), create_graph=True)[0]
+           physics = d2y_pred + d * dy_pred + a * yhp + b * torch.pow(yhp, 3) - gamma * torch.cos(w * x_physics)
+           loss_physics = (1e-4) * torch.mean(physics**2)
 
-            physics = d2y_pred + d * dy_pred + a * yhp + b * torch.pow(yhp, 3) - gamma * torch.cos(w * x_physics)
-            loss_physics = (1e-4) * torch.mean(physics**2)
-            return loss_physics
+           # Add to total loss
+           total_loss += loss_physics
+
+       return total_loss # Return total loss
 
